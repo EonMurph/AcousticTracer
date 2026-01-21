d922ee239f9ede08e9a23a243cfe7c696c8383f8
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-01-21 12:12:42 +0000

    initial definition of ray.h functions + types

---
+++ core/src/at_ray.h
@@ -0,0 +1,64 @@
+#ifndef AT_RAY_H
+#define AT_RAY_H
+
+#include "at_math.h"
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct {
+    AT_Vec3 origin;
+    AT_Vec3 direction;
+    float energy;
+    float total_distance;
+    uint32_t ray_id;
+    uint32_t bounce_count;
+} AT_Ray;
+
+typedef struct {
+    AT_Vec3 position;
+    AT_Vec3 normal;
+    float t;
+} AT_RayHit;
+
+static inline AT_Ray AT_ray_init(
+    const AT_Vec3 *origin,
+    const AT_Vec3 *direction,
+    float energy,
+    uint32_t ray_id
+) {
+    AT_Ray ray = {
+        .origin = *origin,
+        .direction = AT_vec3_normalize(*direction),
+        .energy = energy,
+        .total_distance = 0.0f,
+        .ray_id = ray_id,
+        .bounce_count = 0,
+    };
+    return ray;
+}
+
+static inline AT_Vec3 AT_ray_at(
+    const AT_Ray *ray,
+    float t
+) {
+    return (AT_vec3_add(ray->origin, AT_vec3_scale(ray->direction, t)));
+}
+
+static inline AT_Vec3 AT_ray_reflect(
+    AT_Vec3 incident,
+    AT_Vec3 normal
+) {
+    return (AT_Vec3){0}; //TODO: Impelement ray reflection
+}
+
+
+bool AT_ray_triangle_intersect(
+    const AT_Ray *ray,
+    const AT_Triangle *triangle, //TODO: define AT_Triangle somewhere
+    AT_RayHit *out_hit
+);
+
+
+
+#endif //AT_RAY_H
