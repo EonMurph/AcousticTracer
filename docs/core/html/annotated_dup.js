76af29db2b27d674d849137efb870b287ff2e143
EonMurph <stiger.home@gmail.com>
2026-01-24 12:31:40 +0000

    Add more public source code documentation

--- docs/core/html/annotated_dup.js
+++ docs/core/html/annotated_dup.js
@@ -1,8 +1,6 @@
 var annotated_dup =
 [
     [ "AT_AABB", "struct_a_t___a_a_b_b.html", null ],
-    [ "AT_Ray", "struct_a_t___ray.html", null ],
-    [ "AT_RayHit", "struct_a_t___ray_hit.html", null ],
     [ "AT_SceneConfig", "struct_a_t___scene_config.html", null ],
     [ "AT_Settings", "struct_a_t___settings.html", null ],
     [ "AT_Source", "struct_a_t___source.html", null ],
